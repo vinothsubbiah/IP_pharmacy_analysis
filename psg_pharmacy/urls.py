5ce376d93c9b6846d9abf88299c7d71f96751a75
velu <velmani@bmesolutions.in>
2022-04-27 10:52:02 +0530

    linked all pags

--- psg_pharmacy/urls.py
+++ psg_pharmacy/urls.py
@@ -2,5 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('dashboard/', views.dashboard, name='index'),
+    path('dashboard/', views.dashboard, name='dashboard'),
+    path('details/', views.details, name='details'),
+    path('index/', views.index, name='index'),
+    path('login/', views.login, name='login'),
+    path('requests/', views.requests, name='requests'),
+    path('alerts/', views.alerts, name='alerts'),
+    path('alerts_info/', views.alerts_info, name='alerts_info'),
 ]
--- psg_pharmacy/views.py
+++ psg_pharmacy/views.py
@@ -1,7 +1,24 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from . import views
+
 
 def dashboard(request):
     return render(request,'dashboard.html')
 # Create your views here.
+def login(request):
+    return render(request, 'login.html')
+
+def index(request):
+    return render(request, 'index.html')
+
+def details(request):
+    return render(request, 'details.html')
+
+def alerts(request):
+    return render(request, 'alerts.html')
+
+def requests(request):
+    return render(request, 'requests.html')
+
+def alerts_info(request):
+    return render(request, 'alerts-info.html')
